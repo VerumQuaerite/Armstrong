824a9fbcc14d3aac05d23b4b00170b9adb3026ca
VerumQuaerite <kaihngje.fixedgear49@gmail.com>
2025-01-22 13:35:47 -0500

    Feat: arrange paragraph

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,13 @@ use std::io;
 
 fn bilangan_armstrong(n: u32) -> bool {
     let digit: Vec<u32> = n.to_string()
-                              .chars()
-                              .map(|c| c.to_digit(10).unwrap())
-                              .collect();
+        .chars()
+        .map(|c| c.to_digit(10).unwrap())
+        .collect();
     let pangkat = digit.len() as u32;
     let pertambahan_pangkat: u32 = digit.iter()
-                                   .map(|&digit| digit.pow(pangkat))
-                                   .sum();
+        .map(|&digit| digit.pow(pangkat))
+        .sum();
     pertambahan_pangkat == n
 }
 
